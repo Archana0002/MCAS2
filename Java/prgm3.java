1c782325e39dc74dd3c7c370e5d5c02b85e020a4
Archana prakasan <85437105+Archana0002@users.noreply.github.com>
2024-07-17 17:14:49 +0530

    Update prgm3.java

--- Java/prgm3.java
+++ Java/prgm3.java
@@ -1,30 +1,39 @@
-class comp
-{
+import java.util.Scanner;
+class comp{
     double real;
     double imag;
-    comp(double s,double b)
+    comp(double r,double i)
     {
-        real=s;
-        imag=b;
+        real=r;
+        imag=i;
     }
 }
-class Main
-{
+class Main{
     public static void main(String args[])
     {
-        comp c1=new comp(1.2,3.3);
-        comp c2=new comp(3.8,3.6);
-        comp result=add(c1,c2);
-        System.out.println("Sum is: " + result.real + "+" + result.imag + "i");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter real part for complex number 1");
+        double r1 = sc.nextDouble();
+        System.out.println("enter imaginary part for complex number1:");
+        double m1 = sc.nextDouble();
+        System.out.println("enter real part for complex number 1");
+        double r2 = sc.nextDouble();
+        System.out.println("enter imaginary part for complex number1:");
+        double m2 = sc.nextDouble();
+        
+        comp c1 = new comp(r1,m1);
+        comp c2 = new comp(r2,m2);
+        comp result;
+        result = add(c1,c2);
+        System.out.println("\n \t sum is " + result.real + "+" + result.imag + "i" );
     }
     static comp add(comp n1,comp n2)
     {
-        comp res=new comp(0.0,0.0);
-        res.real=n1.real+n2.real;
-        res.imag=n1.imag+n2.imag;
-        System.out.println("Complex number1 is: "+n1.real+"+"+n1.imag+"i");
-        System.out.println("Complex number2 is: "+n2.real+"+"+n2.imag+"i");
-        
+        comp res = new comp(0.0,0.0);
+        res.real = n1.real + n2.real;
+        res.imag = n1.imag + n2.imag;
+        System.out.println("complex number 1 is:\t" + n1.real + "+" + n1.imag);
+        System.out.println("complex number 2 is: \t" + n2.real + "+" + n2.imag);
         return res;
     }
 }
